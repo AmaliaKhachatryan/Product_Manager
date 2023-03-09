2bbc943c63dc2ad9f9007e5671b385dcdaaccc97
Amalia Khachatryan <amalia-khachatryan@mail.ru>
2023-03-09 17:46:08 +0300

    Version 2

--- src/test/java/product/ProductManagerTest.java
+++ src/test/java/product/ProductManagerTest.java
@@ -5,19 +5,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ProductManagerTest {
+    Smartphone product1 = new Smartphone(1, "Superphone", 100_000, "xxx");
+    Smartphone product2 = new Smartphone(2, "Telephone", 100_000, "xxx");
+    Smartphone product3 = new Smartphone(3, "phone", 100_000, "xxx");
+    Smartphone product4 = new Smartphone(4, "phone", 100_000, "xxx");
+    Book product5 = new Book(5, "book", 100_000, "xxx");
+    Book product6 = new Book(6, "book", 100_000, "xxx");
+    Book product7 = new Book(7, "book", 100_000, "xxx");
+    Book product8 = new Book(8, "book", 100_000, "xxx");
+    Product product9 = new Product(9, "player", 100_000);
+    Product product10 = new Product(10, "player", 100_000);
     Smartphone smartphone = new Smartphone(1, "phone", 100_000, "samsung");
     Book book = new Book(5, "book", 100_000, "Alexander");
     Product product = new Product(9, "player", 100_000);
-    Smartphone product1 = new Smartphone(1, "Superphone", 100_000, "aaa");
-    Smartphone product2 = new Smartphone(2, "Telephone", 100_000, "bbb");
-    Smartphone product3 = new Smartphone(3, "phone", 100_000, "ccc");
-    Smartphone product4 = new Smartphone(4, "phone", 100_000, "ddd");
-    Book product5 = new Book(5, "book", 100_000, "ggg");
-    Book product6 = new Book(6, "book", 100_000, "eee");
-    Book product7 = new Book(7, "book", 100_000, "fff");
-    Book product8 = new Book(8, "book", 100_000, "ooo");
-    Product product9 = new Product(9, "player", 100_000);
-    Product product10 = new Product(10, "player", 100_000);
     RepositoryProduct repo = new RepositoryProduct();
     ProductManager manager = new ProductManager(repo);
 
@@ -75,4 +75,49 @@ class ProductManagerTest {
         Assertions.assertEquals(true, manager.matches(product1, "phone"));
     }
 
+    @Test
+    public void searchByOneProductTest() {
+        RepositoryProduct repo = new RepositoryProduct();
+        ProductManager manager = new ProductManager(repo);
+        manager.add(book);
+        Product[] expected = {book};
+        Assertions.assertArrayEquals(expected, manager.searchBy("book"));
+    }
+
+    @Test
+    public void searchByZeroProductTest() {
+        RepositoryProduct repo = new RepositoryProduct();
+        ProductManager manager = new ProductManager(repo);
+        Product[] expected = {};
+        Assertions.assertArrayEquals(expected, manager.searchBy("player"));
+    }
+
+    @Test
+    public void searchBySmartphoneProductTest() {
+        RepositoryProduct repo = new RepositoryProduct();
+        ProductManager manager = new ProductManager(repo);
+        manager.add(smartphone);
+        Product[] expected = {smartphone};
+        Assertions.assertArrayEquals(expected, manager.searchBy("pho"));
+    }
+
+    @Test
+    public void searchBySmartphoneTest() {
+        RepositoryProduct repo = new RepositoryProduct();
+        ProductManager manager = new ProductManager(repo);
+        manager.add(smartphone);
+        Product[] expected = {};
+        Assertions.assertArrayEquals(expected, manager.searchBy("book"));
+    }
+
+    @Test
+    public void searchByAllProductTest() {
+        RepositoryProduct repo = new RepositoryProduct();
+        ProductManager manager = new ProductManager(repo);
+        manager.add(smartphone);
+        manager.add(book);
+        manager.add(product);
+        Product[] expected = {smartphone};
+        Assertions.assertArrayEquals(expected, manager.searchBy("ph"));
+    }
 }
